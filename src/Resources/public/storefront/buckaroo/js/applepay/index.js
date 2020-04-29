2b07526368fa25eaef0d70900c85ceadec200400
vladislav_padalka_hys <vladislav.padalka@hys-enterprise.com>
2020-04-29 10:46:00 +0300

    BP-155 Shopware 6 - Add payment method Apple Pay

--- src/Resources/public/storefront/buckaroo/js/applepay/index.js
+++ src/Resources/public/storefront/buckaroo/js/applepay/index.js
@@ -1,43 +1,40 @@
 import ApplePay from './applepay.js';
 
-if ($('.applepay-button-container')[0]) {
+function loadBuckarooApple() {
 
-    function loadBuckarooApple() {
+    const loadBuckarooSdk = new Promise((resolve) => {
+        var script = document.createElement("script");
+        script.src = "https://checkout.buckaroo.nl/api/buckaroosdk/script/en-US";
+        script.async = true;
+        document.head.appendChild(script);
+        script.onload = () => {
+            resolve();
+        };
+    });
 
-        const loadBuckarooSdk = new Promise((resolve) => {
-            var script = document.createElement("script");
-            script.src = "https://checkout.buckaroo.nl/api/buckaroosdk/script/en-US";
-            script.async = true;
-            document.head.appendChild(script);
-            script.onload = () => {
-                resolve();
-            };
-        });
+    loadBuckarooSdk.then(() => {
+        const applepay = new ApplePay;
+        applepay.init();
+    });
 
-        loadBuckarooSdk.then(() => {
-            const applepay = new ApplePay;
-            applepay.init();
-        });
+}
 
-    }
+if ((typeof jQuery == 'undefined') || (typeof jQuery.ajax == 'undefined')) {
+    //console.log("====applepay====jQuery1");
+    const loadJquery = new Promise((resolve) => {
+        var script = document.createElement("script");
+        script.src = "https://code.jquery.com/jquery-3.2.1.min.js";
+        script.async = true;
+        document.head.appendChild(script);
+        script.onload = () => {
+            resolve();
+        };
+    });
 
-    if ((typeof jQuery == 'undefined') || (typeof jQuery.ajax == 'undefined')) {
-        //console.log("====applepay====jQuery1");
-        const loadJquery = new Promise((resolve) => {
-            var script = document.createElement("script");
-            script.src = "https://code.jquery.com/jquery-3.2.1.min.js";
-            script.async = true;
-            document.head.appendChild(script);
-            script.onload = () => {
-                resolve();
-            };
-        });
-
-        loadJquery.then(() => {
-            loadBuckarooApple();
-        });
-    } else {
+    loadJquery.then(() => {
         loadBuckarooApple();
-    }
+    });
+} else {
+    loadBuckarooApple();
 }
 
