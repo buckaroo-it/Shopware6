7b749b80c6e044bbad635881da0ec78ac6c5cd55
Sergey Saranchuk <sergey.saranchuk@hys-enterprise.com>
2020-10-06 17:54:17 +0300

    BP-449, BP-450 AfterPay: Wrong error message for incorrect

--- src/Resources/app/storefront/src/helper/buckaroo-payment.helper.js
+++ src/Resources/app/storefront/src/helper/buckaroo-payment.helper.js
@@ -11,6 +11,22 @@ export default class BuckarooPaymentHelper extends Plugin {
             }
         }
 
+        const buckarooMobileInputs = ['buckarooAfterpayPhone','buckarooIn3Phone'];
+        for (const fieldId of buckarooMobileInputs) {
+            const field = document.getElementById(fieldId);
+            if (field) {
+                field.addEventListener('change', this._handleMobileInputChanged.bind(this));
+            }
+        }
+
+        const buckarooDoBInputs = ['buckaroo_afterpay_DoB','buckaroo_capayablein3_DoB'];
+        for (const fieldId of buckarooDoBInputs) {
+            const field = document.getElementById(fieldId);
+            if (field) {
+                field.addEventListener('change', this._handleDoBInputChanged.bind(this));
+            }
+        }
+
         const field = document.getElementById('P24Currency');
         if (field) {
             if(field.value != 'PLN'){
@@ -48,5 +64,22 @@ export default class BuckarooPaymentHelper extends Plugin {
             default:
         }
 
+    }
+
+    _handleMobileInputChanged(event) {
+        document.getElementById('buckarooMobilePhoneError').style.display = 'none';
+        if(!event.target.value.match(/^\d{10}$/)){
+            document.getElementById('buckarooMobilePhoneError').style.display = 'block';
+        }
+    }
+
+    _handleDoBInputChanged(event) {
+        document.getElementById('buckarooDoBPhoneError').style.display = 'none';
+        const fieldValue = event.target.value;
+        const x = new Date(Date.parse(fieldValue));  
+        const Cnow = new Date();
+        if ((Cnow.getFullYear() - x.getFullYear() < 18) || x.getFullYear() < 1900){
+            document.getElementById('buckarooDoBPhoneError').style.display = 'block';
+        }  
     }
 }
