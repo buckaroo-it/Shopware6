ee2bc670ec6f9524976871f60cccb5e4edf050b4
Sergey Saranchuk <sergey.saranchuk@hys-enterprise.com>
2020-10-14 16:22:28 +0300

    BP-449 add French Translations

---
+++ src/Resources/snippet/fr_FR/FrenchTranslations.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace Buckaroo\Shopware6\Resources\snippet\fr_FR;
+
+use Shopware\Core\System\Snippet\Files\SnippetFileInterface;
+
+class FrenchTranslations implements SnippetFileInterface
+{
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'messages.fr-FR';
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return __DIR__ . '/messages.fr-FR.json';
+    }
+
+    /**
+     * @return string
+     */
+    public function getIso(): string
+    {
+        return 'fr-FR';
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthor(): string
+    {
+        return 'Buckaroo';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isBase(): bool
+    {
+        return false;
+    }
+}
