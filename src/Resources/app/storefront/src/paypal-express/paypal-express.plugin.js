29c0255fdacae1579181174e982886a3a196e4e1
Ivascu Madalin <madalin.ivascu@arnia.ro>
2022-09-06 08:58:27 +0200

    Added complete functionality

--- src/Resources/app/storefront/src/paypal-express/paypal-express.plugin.js
+++ src/Resources/app/storefront/src/paypal-express/paypal-express.plugin.js
@@ -10,7 +10,7 @@ export default class PaypalExpressPlugin extends Plugin {
     }
     httpClient = new HttpClient();
 
-    
+
     url = '/buckaroo';
     /**
      * buckaroo sdk
@@ -18,6 +18,8 @@ export default class PaypalExpressPlugin extends Plugin {
     sdk;
 
     result = null;
+
+    cartToken;
 
     sdkOptions = {
         containerSelector: ".buckaroo-paypal-express",
@@ -30,7 +32,6 @@ export default class PaypalExpressPlugin extends Plugin {
         onSuccessCallback: this.onSuccessCallback.bind(this),
         onErrorCallback: this.onErrorCallback.bind(this),
         onCancelCallback: this.onCancelCallback.bind(this),
-        onInitCallback: this.onInitCallback.bind(this),
         onClickCallback: this.onClickCallback.bind(this),
     }
 
@@ -62,17 +63,14 @@ export default class PaypalExpressPlugin extends Plugin {
 
     init() {
 
-
         if (this.merchantId === null) {
             alert('Merchant id is required');
         }
-        
 
         this.loadJquery().then(() => {
             this.loadSdk().then(() => {
                 this.sdk = BuckarooSdk.PayPal;
                 this.sdk.initiate(this.sdkOptions);
-                this.listen();
             })
         })
 
@@ -86,15 +84,17 @@ export default class PaypalExpressPlugin extends Plugin {
         let shipping = this.setShipping(data);
         return shipping.then((response) => {
             if (response.error === false) {
-                this.sdkOptions.amount = response.data.value.value
+                this.cartToken = response.token;
+                this.sdkOptions.amount = response.cart.value
                 return actions.order.patch([
                     {
                         op: 'replace',
                         path: '/purchase_units/@reference_id==\'default\'/amount',
-                        value: response.data.value
+                        value: response.cart
                     }
                 ]);
             } else {
+                this.displayErrorMessage(response.message);
                 actions.reject(response.message);
             }
         })
@@ -106,21 +106,17 @@ export default class PaypalExpressPlugin extends Plugin {
         if (this.result.error === true) {
             this.displayErrorMessage(message);
         } else {
-            if (this.result.data.redirect) {
-                window.location = this.result.data.redirect;
+            if (this.result.redirect) {
+                window.location = this.result.redirect;
             } else {
                 this.displayErrorMessage(this.options.i18n.cannot_create_payment);
             }
         }
-        console.log('onSuccessCallback');
     }
 
     onErrorCallback(reason) {
         // custom error behavior
         this.displayErrorMessage(reason);
-    }
-    onInitCallback() {
-        this.get_cart_total();
     }
     onCancelCallback() {
         this.displayErrorMessage(this.options.i18n.cancel_error_message)
@@ -132,56 +128,27 @@ export default class PaypalExpressPlugin extends Plugin {
     }
 
     /**
-     * listen to any change in the cart and get total
-     */
-    listen() {
-        // $(".cart .quantity input").on('change', () => {
-        //     this.get_cart_total();
-        // });
-
-        // $(".variations_form").on("show_variation hide_variation", () => {
-        //     this.get_cart_total();
-        // });
-        // $(document.body).on('wc_fragments_refreshed updated_shipping_method', () => {
-        //     this.get_cart_total();
-        //     if ($(".buckaroo-paypal-express").length) {
-        //         this.sdk.initiate(this.sdkOptions);
-        //     }
-        // });
-    }
-    /**
-     * Get cart total to output in paypal
-     */
-    get_cart_total() {
-        
-        // $.post(this.url, {
-        //     action: 'buckaroo_paypal_express_get_cart_total',
-        //     order_data: this.getOrderData(),
-        //     page: this.page
-        // })
-        //     .then((response) => {
-        //         if (response.data) {
-        //             this.sdkOptions.amount = response.data.total
-        //         }
-        //     })
-    }
-
-    /**
      * Create order and do payment
      * @param {string} orderId 
      * @returns Promise
      */
     createTransaction(orderId) {
+
+        let data = {
+            orderId,
+            _csrf_token: this.options.csrf.pay
+        };
+
+        if (this.cartToken) {
+            data.cartToken = this.cartToken;
+        }
         return new Promise((resolve) => {
             this.httpClient.post(
                 `${this.url}/paypal/pay`,
-                JSON.stringify({
-                     orderId,
-                    _csrf_token: this.options.csrf.pay
-                }),
+                JSON.stringify(data),
                 (response) => {
-                    this.result = response;
-                    resolve(response);
+                    this.result = JSON.parse(response);
+                    resolve(JSON.parse(response));
                 })
         })
     }
@@ -194,7 +161,8 @@ export default class PaypalExpressPlugin extends Plugin {
      */
     setShipping(data) {
         let formData = null;
-        if(this.options.page === 'product') {
+
+        if (this.options.page === 'product') {
             formData = FormSerializeUtil.serializeJson(this.el.closest('form'));
         }
         return new Promise((resolve) => {
@@ -206,7 +174,7 @@ export default class PaypalExpressPlugin extends Plugin {
                     order: data,
                     page: this.options.page
                 }), (response) => {
-                    resolve(response);
+                    resolve(JSON.parse(response));
                 }
             )
         });
@@ -217,20 +185,27 @@ export default class PaypalExpressPlugin extends Plugin {
      * @param {string} message 
      */
     displayErrorMessage(message) {
-        console.log(message);
-    //     $('.buckaroo-paypal-express-error').remove();
-    //     if (typeof message === 'object') {
-    //         console.log(message);
-    //         message = buckaroo_paypal_express.i18n.cannot_create_payment;
-    //     }
-    //     const content = `      
-    //     <div class="woocommerce-error buckaroo-paypal-express-error" role="alert">
-    //       ${message}
-    //     </div>
-    //   `;
-    //     $('.woocommerce-notices-wrapper').first().prepend(content);
-    //     setTimeout(function () {
-    //         $('.buckaroo-paypal-express-error').fadeOut(1000);
-    //     }, 10000);
+        $('.buckaroo-paypal-express-error').remove();
+        if (typeof message === 'object') {
+            message = buckaroo_paypal_express.i18n.cannot_create_payment;
+        }
+        const content = `
+        <div role="alert" class="alert alert-warning alert-has-icon buckaroo-paypal-express-error">
+            <span class="icon icon-warning">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24"><defs><path d="m13.7744 1.4124 9.7058 18.6649c.5096.98.1283 2.1875-.8517 2.6971a2 2 0 0 1-.9227.2256H2.2942c-1.1045 0-2-.8954-2-2a2 2 0 0 1 .2256-.9227l9.7058-18.665c.5096-.98 1.7171-1.3613 2.6971-.8517a2 2 0 0 1 .8517.8518zM2.2942 21h19.4116L12 2.335 2.2942 21zM12 17c.5523 0 1 .4477 1 1s-.4477 1-1 1-1-.4477-1-1 .4477-1 1-1zm1-2c0 .5523-.4477 1-1 1s-1-.4477-1-1v-5c0-.5523.4477-1 1-1s1 .4477 1 1v5z" id="icons-default-warning"></path></defs><use xlink:href="#icons-default-warning" fill="#758CA3" fill-rule="evenodd"></use></svg>
+            </span>                                    
+            <div class="alert-content-container"> 
+                <div class="alert-content">
+                    ${message}
+                </div>
+                
+            </div>
+        </div>
+
+      `;
+        $('.flashbags').first().prepend(content);
+        setTimeout(function () {
+            $('.buckaroo-paypal-express-error').fadeOut(1000);
+        }, 10000);
     }
 }
