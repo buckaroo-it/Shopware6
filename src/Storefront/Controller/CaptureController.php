49f453504cc942dc3f6c8b609fb18e7ac04bf90f
Ivascu Madalin <madalin.ivascu@arnia.ro>
2022-11-18 08:32:22 +0100

    Add translation to controllers

--- src/Storefront/Controller/CaptureController.php
+++ src/Storefront/Controller/CaptureController.php
@@ -7,26 +7,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
-use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionEntity;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Controller\StorefrontController;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Buckaroo\Shopware6\Helpers\CheckoutHelper;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
-use Shopware\Core\Checkout\Payment\Exception\AsyncPaymentFinalizeException;
-use Shopware\Core\Checkout\Payment\Exception\AsyncPaymentProcessException;
-use Shopware\Core\Checkout\Payment\Exception\CustomerCanceledAsyncPaymentException;
-use Shopware\Core\Checkout\Payment\Exception\InvalidOrderException;
 
-use Buckaroo\Shopware6\Helpers\Constants\ResponseStatus;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -38,21 +28,13 @@ class CaptureController extends StorefrontController
      */
     private $logger;
     
-    private $transactionRepository;
-    
     private $checkoutHelper;
-    
-    private $orderRepository;
 
     public function __construct(
-        EntityRepositoryInterface $transactionRepository,
         CheckoutHelper $checkoutHelper,
-        EntityRepositoryInterface $orderRepository,
         LoggerInterface $logger
     ) {
-        $this->transactionRepository = $transactionRepository;
         $this->checkoutHelper = $checkoutHelper;
-        $this->orderRepository = $orderRepository;
         $this->logger = $logger;
     }
 
@@ -72,14 +54,20 @@ class CaptureController extends StorefrontController
 
         if (empty($orderId)) {
             $this->logger->info(__METHOD__ . "|5|");
-            return new JsonResponse(['status' => false, 'message' => 'Missing order orderId'], Response::HTTP_NOT_FOUND);
+            return new JsonResponse(
+                ['status' => false, 'message' => $this->trans("buckaroo-payment.missing_order_id")],
+                Response::HTTP_NOT_FOUND
+            );
         }
 
         $order = $this->checkoutHelper->getOrderById($orderId, $context);
 
         if (null === $order) {
             $this->logger->info(__METHOD__ . "|10|");
-            return new JsonResponse(['status' => false, 'message' => 'Order transaction not found'], Response::HTTP_NOT_FOUND);
+            return new JsonResponse(
+                ['status' => false, 'message' => $this->trans("buckaroo-payment.missing_transaction")],
+                Response::HTTP_NOT_FOUND
+            );
         }
 
         $this->logger->info(__METHOD__ . "|15|", [$orderId]);
@@ -106,7 +94,7 @@ class CaptureController extends StorefrontController
         return new JsonResponse(
             [
                 'status'  => false,
-                'message' => 'Unfortunately an error occurred while processing your refund. Please try again.',
+                'message' => $this->trans("buckaroo-payment.capture.general_capture_error"),
                 'code'    => 0,
             ],
             Response::HTTP_BAD_REQUEST
--- src/Storefront/Controller/RefundController.php
+++ src/Storefront/Controller/RefundController.php
@@ -6,49 +6,30 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
-use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionEntity;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Controller\StorefrontController;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Buckaroo\Shopware6\Helpers\CheckoutHelper;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 use Exception;
-use Shopware\Core\Checkout\Payment\Exception\AsyncPaymentFinalizeException;
-use Shopware\Core\Checkout\Payment\Exception\AsyncPaymentProcessException;
-use Shopware\Core\Checkout\Payment\Exception\CustomerCanceledAsyncPaymentException;
-use Shopware\Core\Checkout\Payment\Exception\InvalidOrderException;
 
-use Buckaroo\Shopware6\Helpers\Constants\ResponseStatus;
 
 /**
  */
 class RefundController extends StorefrontController
 {
-    /** @var LoggerInterface */
-    private $logger;
-    
-    private $transactionRepository;
     
     private $checkoutHelper;
     
-    private $orderRepository;
 
     public function __construct(
-        EntityRepositoryInterface $transactionRepository,
-        CheckoutHelper $checkoutHelper,
-        EntityRepositoryInterface $orderRepository
+        CheckoutHelper $checkoutHelper
     ) {
-        $this->transactionRepository = $transactionRepository;
         $this->checkoutHelper = $checkoutHelper;
-        $this->orderRepository = $orderRepository;
     }
 
     /**
@@ -66,13 +47,19 @@ class RefundController extends StorefrontController
         $orderItems = $request->get('orderItems');
 
         if (empty($orderId)) {
-            return new JsonResponse(['status' => false, 'message' => 'Missing order orderId'], Response::HTTP_NOT_FOUND);
+            return new JsonResponse(
+                ['status' => false, 'message' => $this->trans("buckaroo-payment.missing_order_id")],
+                Response::HTTP_NOT_FOUND
+            );
         }
 
         $order = $this->checkoutHelper->getOrderById($orderId, $context);
 
         if (null === $order) {
-            return new JsonResponse(['status' => false, 'message' => 'Order transaction not found'], Response::HTTP_NOT_FOUND);
+            return new JsonResponse(
+                ['status' => false, 'message' => $this->trans("buckaroo-payment.missing_transaction")],
+                Response::HTTP_NOT_FOUND
+            );
         }
         $responses = [];
         try {
