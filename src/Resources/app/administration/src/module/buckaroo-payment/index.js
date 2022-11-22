7fda3ed27bcdb4fcfca283c7a480d405f12b9b46
Ivascu Madalin <madalin.ivascu@arnia.ro>
2022-11-22 15:25:08 +0100

    Remove depreciated code

--- src/Resources/app/administration/src/module/buckaroo-payment/index.js
+++ src/Resources/app/administration/src/module/buckaroo-payment/index.js
@@ -3,7 +3,6 @@ const { Module } = Shopware;
 import './page/buckaroo-settings';
 
 import './extension/sw-order';
-import './extension/sw-plugin';
 import './extension/sw-settings-index';
 import './extension/sw-order-detail-base';
 import './extension/sw-order-user-card';
@@ -42,7 +41,6 @@ Module.register('buckaroo-payment', {
         next(currentRoute);
     },
 
-    routes: {
         index: {
             component: 'buckaroo-settings',
             path: 'index',
