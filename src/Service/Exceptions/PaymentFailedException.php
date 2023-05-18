b2a4f41e4712de0055cbb01d62f05856db348440
Ivascu Madalin <madalin.ivascu@arnia.ro>
2023-05-18 10:38:51 +0200

    BP-2562 User is not redirected to Payment section after rejected/failed iDEAL order

--- src/Service/Exceptions/PaymentFailedException.php
+++ src/Service/Exceptions/PaymentFailedException.php
@@ -8,8 +8,15 @@ use Shopware\Core\Checkout\Payment\Exception\PaymentProcessException;
 
 class PaymentFailedException extends PaymentProcessException
 {
+    protected $statusCode;
+
     public function getErrorCode(): string
     {
-        return 'PAYMENT_FAILED_ERROR';
+        return 'PAYMENT_FAILED_ERROR_' . $this->statusCode;
+    }
+
+    public function setPaymentStatusCode(string $statusCode): void
+    {
+        $this->statusCode = $statusCode;
     }
 }
