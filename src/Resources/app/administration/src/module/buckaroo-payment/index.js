a4cc7e248223ae333c174196fac9a6affb1f9c1a
Ivascu Madalin <madalin.ivascu@arnia.ro>
2023-06-06 11:39:16 +0200

    BP-2613 Admin and payment proces issue/error for specific merchant

--- src/Resources/app/administration/src/module/buckaroo-payment/index.js
+++ src/Resources/app/administration/src/module/buckaroo-payment/index.js
@@ -7,7 +7,6 @@ import './extension/sw-plugin';
 import './extension/sw-settings-index';
 import './extension/sw-order-detail-base';
 import './extension/sw-order-user-card';
-import './extension/sw-context-menu-item';
 import './page/buckaroo-payment-detail';
 
 import nlNL from './snippet/nl-NL.json';
