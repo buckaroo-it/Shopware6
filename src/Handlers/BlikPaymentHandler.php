7069d2f0d4e8f019cac4dea5c6e6bffb90a06ca9
g.prenaj <gentianprenaj@gmail.com>
2024-07-02 13:55:35 +0200

    fixes

---
+++ src/Handlers/BlikPaymentHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buckaroo\Shopware6\Handlers;
+
+use Buckaroo\Shopware6\PaymentMethods\Blik;
+
+class BlikPaymentHandler extends AsyncPaymentHandler
+{
+    protected string $paymentClass = Blik::class;
+
+}
