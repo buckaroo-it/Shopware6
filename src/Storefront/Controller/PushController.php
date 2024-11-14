e59e116dacc8026df2f8d2f3a14b5ad4a959efc7
g.prenaj <gentianprenaj@gmail.com>
2024-11-14 20:04:40 +0100

    ideal cancellation issue

--- src/Storefront/Controller/PushController.php
+++ src/Storefront/Controller/PushController.php
@@ -379,7 +379,7 @@ class PushController extends StorefrontController
             )) {
                 return $this->response('buckaroo.messages.skippedPush');
             }
-            $this->setPaymentState("cancelled", $orderTransactionId, $salesChannelContext, $request);
+            $this->setPaymentState("process_unconfirmed", $orderTransactionId, $salesChannelContext, $request);
 
             return $this->response('buckaroo.messages.orderCancelled');
         }
