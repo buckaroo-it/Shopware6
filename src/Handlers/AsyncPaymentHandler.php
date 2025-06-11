6b8352806082bded9be55edaa93fa86e30f1925c
g.prenaj <gentianprenaj@gmail.com>
2025-06-11 13:19:00 +0200

    fix spacing

--- src/Handlers/AsyncPaymentHandler.php
+++ src/Handlers/AsyncPaymentHandler.php
@@ -208,7 +208,9 @@ class AsyncPaymentHandler implements AsynchronousPaymentHandlerInterface
         $this->setFeeOnOrder($transaction, $salesChannelContext, $paymentCode);
     }
 
-    private function handleRedirectResponse(AsyncPaymentTransactionStruct $transaction): void{
+    private function handleRedirectResponse(
+        AsyncPaymentTransactionStruct $transaction
+    ): void {
         $this->asyncPaymentService
             ->checkoutHelper
             ->getSession()
