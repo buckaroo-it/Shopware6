0e8ac503f534f613f60789c91f325685ea186ec3
g.prenaj <gentianprenaj@gmail.com>
2025-08-25 14:02:29 +0200

    stan validation fixes

--- src/PaymentMethods/Alipay.php
+++ src/PaymentMethods/Alipay.php
@@ -8,80 +8,80 @@ use Buckaroo\Shopware6\Handlers\AlipayPaymentHandler;
 
 class Alipay extends AbstractPayment
 {
-	   /*
-	   * @return string
-	   */
-	   public function getBuckarooKey(): string
-	   {
-	       return 'Alipay';
-	   }
+    /*
+    * @return string
+    */
+    public function getBuckarooKey(): string
+    {
+        return 'Alipay';
+    }
 
-	   /**
-	    * {@inheritDoc}
-	    *
-	    * @return string
-	    */
-	   public function getName(): string
-	   {
-	       return 'Alipay';
-	   }
+    /**
+     * {@inheritDoc}
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'Alipay';
+    }
 
-	   /**
-	    * {@inheritDoc}
-	    *
-	    * @return string
-	    */
-	   public function getDescription(): string
-	   {
-	       return 'Pay with Alipay';
-	   }
+    /**
+     * {@inheritDoc}
+     *
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return 'Pay with Alipay';
+    }
 
-	   /**
-	    * {@inheritDoc}
-	    *
-	    * @return string
-	    */
-	   public function getMedia(): string
-	   {
-	       return __DIR__  . '/../Resources/views/storefront/buckaroo/payments/alipay.svg';
-	   }
+    /**
+     * {@inheritDoc}
+     *
+     * @return string
+     */
+    public function getMedia(): string
+    {
+        return __DIR__  . '/../Resources/views/storefront/buckaroo/payments/alipay.svg';
+    }
 
-	   /**
-	    * {@inheritDoc}
-	    *
-	    * @return string
-	    */
-	   public function getPaymentHandler(): string
-	   {
-	       return AlipayPaymentHandler::class;
-	   }
+    /**
+     * {@inheritDoc}
+     *
+     * @return string
+     */
+    public function getPaymentHandler(): string
+    {
+        return AlipayPaymentHandler::class;
+    }
 
-	   /**
-	    * {@inheritDoc}
-	    *
-	    * @return string|null
-	    */
-	   public function getTemplate(): ?string
-	   {
-	       return null;
-	   }
+    /**
+     * {@inheritDoc}
+     *
+     * @return string|null
+     */
+    public function getTemplate(): ?string
+    {
+        return null;
+    }
 
-	   /**
-	    * {@inheritDoc}
-	    *
-	    * @return array<mixed>
-	    */
-	   public function getTranslations(): array
-	   {
-	       return [
-	           'de-DE' => [
-	               'name'        => $this->getName(),
-	               'description' => 'Bezahlen mit Alipay',
-	           ],
-	           'en-GB' => [
-	               'name'        => $this->getName(),
-	               'description' => $this->getDescription(),
-	           ],
-	       ];
-	   }
+    /**
+     * {@inheritDoc}
+     *
+     * @return array<mixed>
+     */
+    public function getTranslations(): array
+    {
+        return [
+            'de-DE' => [
+                'name'        => $this->getName(),
+                'description' => 'Bezahlen mit Alipay',
+            ],
+            'en-GB' => [
+                'name'        => $this->getName(),
+                'description' => $this->getDescription(),
+            ],
+        ];
+    }
 }
