cfe510cda942da0aa8d4d92b42c9f61118ea5e00
Martijn de Dood <martijn@dutchdrops.com>
2025-08-28 13:32:16 +0200

    fix: rollback array index

--- src/Handlers/In3V2.php
+++ src/Handlers/In3V2.php
@@ -99,9 +99,9 @@ class In3V2
         ];
 
         if (isset($streetData['number_addition']) && strlen($streetData['number_addition']) > 0) {
-            $data['houseNumberAdditional'] = $streetData['number_addition'];
+            $data['address']['houseNumberAdditional'] = $streetData['number_addition'];
         } elseif (strlen($address->getAdditionalAddressLine2()) > 0) {
-            $data['houseNumberAdditional'] = $address->getAdditionalAddressLine2();
+            $data['address']['houseNumberAdditional'] = $address->getAdditionalAddressLine2();
         }
 
         return $data;
