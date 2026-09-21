0ed95224fb2779e9831e15926e34b4903bc4c204
g.prenaj <gentianprenaj@gmail.com>
2026-09-21 17:02:48 +0200

    php stan errors and warnings

--- src/BuckarooPayments.php
+++ src/BuckarooPayments.php
@@ -13,8 +13,6 @@ use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Shopware\Core\Framework\Plugin\Context\UpdateContext;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class BuckarooPayments extends Plugin
 {
@@ -28,8 +26,10 @@ class BuckarooPayments extends Plugin
         // Handle class aliasing for Shopware 6.5+ compatibility
         $this->handlePaymentHandlerCompatibility();
 
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/Resources/config'));
-        $loader->load('services.xml');
+        // Resources/config/services.xml is already loaded by parent::build(),
+        // which runs Shopware\Core\Framework\Bundle::registerContainerFile()
+        // over Resources/config/services.* - loading it again here would only
+        // redefine the same services.
     }
 
     /**
